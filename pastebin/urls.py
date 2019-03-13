824400c49d7018979a74ef86e6122fcd38f31d64
bikramjit <bikramjit@bik.bik>
2019-03-12 21:41:50 -0400

    some other minor fixes

--- pastebin/urls.py
+++ pastebin/urls.py
@@ -24,6 +24,7 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', pastings.views.home, name='home'),
     path('newPaste/', pastings.views.newPaste, name='newPaste'),
+    path('profile/', pastings.views.profile, name='profile'),
     path('accounts/', include('accounts.urls')),
     path('accounts/', include('django.contrib.auth.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
--- pastings/views.py
+++ pastings/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 from .models import Pastings
+from django.shortcuts import redirect
 
 # Create your views here.
 def home(request):
@@ -7,5 +8,12 @@ def home(request):
     return render(request, 'pastings/home.html', {'pastings':pastings})
 
 def newPaste(request):
+    if  request.user.is_authenticated:
+        pastings = Pastings.objects
+        return render(request, 'pastings/paste.html', {'pastings':pastings})
+    else:
+        return redirect('login')
+
+def profile(request):
     pastings = Pastings.objects
-    return render(request, 'pastings/paste.html', {'pastings':pastings})
+    return render(request, 'pastings/profile.html', {'pastings':pastings})
